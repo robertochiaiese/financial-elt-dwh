21e43551e06e263584e36dfa929fd1be428a5a82
Roberto <chiaieseroberto@gmail.com>
2025-11-27 19:22:05 +0100

    Add files via upload

---
+++ airflow/dags/orchestrator.py
@@ -0,0 +1,172 @@
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow import DAG
+from datetime import datetime, timedelta
+from scripts.dbConnect import connect_to_db
+import pandas as pd
+from docker.types import Mount
+from sqlalchemy import create_engine, schema
+import logging
+from pathlib import Path
+from sqlalchemy import Table, Column, Integer, String, MetaData, DECIMAL, Date, DateTime
+from scripts.apirequest import fetch_data
+
+
+DBT_PROJECT_DIR = "C:/Users/rober/Desktop/Project-2-DW/dbt/my_project"
+DBT_PROFILES_YML = "C:/Users/rober/Desktop/Project-2-DW/dbt/profiles.yml"
+
+
+default_args = {
+    'owner':'roberto',
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5)
+}
+
+database = 'marketdata' #os.getenv("POSTGRES_DB")
+usr = 'roberto' #os.getenv("POSTGRES_USER")
+psw = 'password' #os.getenv("POSTGRES_PASSWORD")
+port = '5432' #os.getenv("POSTGRES_PORT")
+host = 'db'
+
+
+def load_to_db(ti, **kwargs):
+    print("Connecting to the database...")
+    engine = connect_to_db(host, database, usr, psw, port)
+    print(f"Succesfully connected to {database}")
+    
+
+    if not engine.dialect.has_schema(engine, "raw"):
+        engine.execute(schema.CreateSchema("raw"))
+
+        metadata = MetaData(schema="raw")
+
+        weekly_prices = Table(
+            'raw_alpha_vantage', metadata,
+            Column('timestamp', Date),
+            Column('open', DECIMAL),
+            Column('high', DECIMAL),
+            Column('low', DECIMAL),
+            Column('close', DECIMAL),
+            Column('volume', DECIMAL),
+            Column('updated_at', DateTime)
+        )
+
+
+        dates = Table('raw_dates', metadata,
+            Column('Id', Integer, primary_key=True),
+            Column('Date', Date),
+            Column('DayLongName', String),
+            Column('MonthLongName', String),
+            Column('CalendarDay', Integer),
+            Column('CalendarWeek', Integer),
+            Column('CalendarWeekStartDateId', Integer, nullable=True),
+            Column('CalendarWeekEndDateId', Integer, nullable=True),
+            Column('CalendarDayInWeek', Integer),
+            Column('CalendarMonth', Integer),
+            Column('CalendarNumberOfDaysInMonth', Integer),
+            Column('CalendarDayInMonth', Integer),
+            Column('CalendarNumberOfDaysInQuarter', Integer),
+            Column('CalendarYear', Integer),
+            Column('FiscalWeek', Integer),
+            Column('FiscalMonth', Integer),
+            Column('FiscalYear', Integer)
+        )
+
+        metadata.create_all(engine)
+
+        path_dim_data = ti.xcom_pull(key='dimdate',task_ids="extracting_daily_data")
+        df_date = pd.read_csv(path_dim_data)
+
+
+        print("Uploading raw_dates into raw schema")
+        df_date.to_sql(
+         name="raw_dates",
+         con=engine,
+         schema='raw',
+         if_exists='replace',
+         index=False
+    )
+    print("Done!")
+    path_market_data = ti.xcom_pull(key='marketdata',task_ids="extracting_daily_data")
+    df_mk = pd.read_csv(path_market_data)
+
+
+    
+
+
+    # # NORMALIZZAZIONE COLONNE
+    df_mk.columns = (
+        df_mk.columns
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+    )
+
+    df_mk["updated_at"] = datetime.now()
+
+    print("Uploading raw_alpha_vantage into raw schema")
+    df_mk.to_sql(
+        name="raw_alpha_vantage",
+        con=engine,
+        schema="raw",
+        if_exists="replace",
+        index=False
+    )
+    print("Done!")
+
+
+
+    print("The data will be soon transformed in the datawarehouse...")
+
+def extract_data(ti):
+      print("Extracting the data...")
+      PATH_MARKET_DATA = fetch_data()
+      ti.xcom_push(key='marketdata', value=PATH_MARKET_DATA)
+      PATH_DATES_DATA = fetch_s3_like()
+      ti.xcom_push(key='dimdate', value=PATH_DATES_DATA)
+      print("Data succesfully extracted!")
+
+def fetch_s3_like():
+     return '/opt/airflow/data/dimdates.csv'
+
+    
+
+with DAG(
+    dag_id='etl_process_03',
+    default_args=default_args,
+    start_date=datetime(2025,11,16),
+    catchup=False
+) as dag:
+    extract= PythonOperator(
+        task_id='extracting_daily_data',
+        python_callable=extract_data
+    )
+
+    load=PythonOperator(
+        task_id="load_data_dwh",
+        python_callable=load_to_db
+    )
+
+    
+    transform = DockerOperator(
+    task_id="transform_data",
+    image='ghcr.io/dbt-labs/dbt-postgres:1.9.latest',
+    command='run',
+    working_dir='/usr/app',
+    mounts=[
+        Mount(source=str(DBT_PROJECT_DIR),
+              target='/usr/app',
+              type='bind'),
+        Mount(source=str(DBT_PROFILES_YML),
+              target='/root/.dbt/profiles.yml',
+              type='bind')
+    ],
+        network_mode='project-2-dw_my-network',
+        docker_url=f'tcp://host.docker.internal:2375'
+,
+    auto_remove='success'
+
+
+    )
+
+extract>>load>>transform
